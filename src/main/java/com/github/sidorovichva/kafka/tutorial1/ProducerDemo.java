e94ad4f295d45f8a5d8c9c993c4e4f5cb1510103
sidorovichva <sidorovichva@gmail.com>
2021-08-05 12:30:29 +0300

    Initial commit

---
+++ src/main/java/com/github/sidorovichva/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,22 @@
+package com.github.sidorovichva.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+
+        //send data
+    }
+}
